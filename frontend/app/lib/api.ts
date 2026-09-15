9a44d03497599a8d02979aa0b6595cf95534754f
Jerry zhiya <jerryglitter399@gmail.com>
2026-09-15 07:52:45 +0100

    Added the correct url on api.ts

--- frontend/app/lib/api.ts
+++ frontend/app/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'NEXT_PUBLIC_API_URL=https://jeremiah-zhiya.onrender.com/api';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://jeremiah-zhiya.onrender.com/api';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
